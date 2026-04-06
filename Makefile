7578fe3c8c4545c226fd3bb58c1fbd6a44f62ef5
maya-taki <g.takiyoshi@gmail.com>
2026-04-06 01:43:11 -0300

    starting things up

---
+++ Makefile
@@ -0,0 +1,8 @@
+NAME = minishell
+CC = cc
+CFLAGS = -Werror -Wextra -Wall
+SRC = main.c
+
+all: $(NAME)
+
+fclean: $
---
+++ includes/shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+# define SHELL_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+
+#endif
