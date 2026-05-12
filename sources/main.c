0e66077de25c69a1052c1f8bde663dbbcbaf6bb1
maya-taki <g.takiyoshi@gmail.com>
2026-05-12 20:20:41 -0300

    FIX: update main.c to improve token handling and command validation

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: mtakiyos <mtakiyos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 18:09:20 by mtakiyos          #+#    #+#             */
-/*   Updated: 2026/05/11 22:44:26 by mtakiyos         ###   ########.fr       */
+/*   Updated: 2026/05/12 20:16:39 by mtakiyos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,27 @@
 int	main(void)
 {
 	t_mini	mini;
-	t_token	*tokens;
 
 	using_history();
 	while (1)
 	{
 		mini.input = readline("Minishell$ ");
-		tokens = lexer(mini.input);
+		mini.tokens = lexer(mini.input);
+		add_history(mini.input);
+		if (!mini.tokens)
+		{
+			ft_printf("Invalid command\n");
+			continue ;
+		}
 		mini.cmd = parser(&mini);
 		if (!mini.cmd)
 		{
-			free_all_cmds(mini.cmd);
-			return (0);
+			free_tokens(mini.tokens);
+			continue ;
 		}
-		if (*mini.input != '\0')
-			add_history(mini.input);
-		free(tokens);
+		if (mini.tokens != NULL)
+			free(mini.input);
+		free(mini.tokens);
 	}
 	clear_history();
 	return (0);
