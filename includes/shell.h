1392af15b5847ca7f33fbe076245d5a6128d1f13
maya-taki <g.takiyoshi@gmail.com>
2026-06-08 00:25:24 -0300

    FIX: norminette function tabs

--- includes/shell.h
+++ includes/shell.h
@@ -6,7 +6,7 @@
 /*   By: mtakiyos <mtakiyos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/08 18:43:36 by mtakiyos          #+#    #+#             */
-/*   Updated: 2026/06/08 00:21:15 by mtakiyos         ###   ########.fr       */
+/*   Updated: 2026/06/08 00:24:46 by mtakiyos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -149,15 +149,15 @@ int				builtin_exit(t_shell *shell);
 
 extern sig_atomic_t	g_signal;
 
-void	handle_sigint(int sig);
-void	setup_signals(void);
-void	setup_exec_signal(void);
-void	setup_child_signal(void);
-int		heredoc_event_hook(void);
-int		prompt_event_hook(void);
-void	handle_sigint_heredoc(int sig);
-void	setup_heredoc_signals(void);
-void 	proper_exit(t_shell *shell);
+void			handle_sigint(int sig);
+void			setup_signals(void);
+void			setup_exec_signal(void);
+void			setup_child_signal(void);
+int				heredoc_event_hook(void);
+int				prompt_event_hook(void);
+void			handle_sigint_heredoc(int sig);
+void			setup_heredoc_signals(void);
+void			proper_exit(t_shell *shell);
 
 /*---------------------------------------------------------------------------*/
 /*  ENV UTILS                                                                */
